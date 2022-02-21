b0d0415888b870bb1189339c5a37b08535021cfd
Anton Patsev <patsev.anton@gmail.com>
2022-02-21 16:02:41 +0600

    Add aws_iam_user_group_membership

--- 1-create-administrators-user-group/main.tf
+++ 1-create-administrators-user-group/main.tf
@@ -7,3 +7,16 @@ resource "aws_iam_group_policy_attachment" "aws_group_Admins_attach" {
   group      = aws_iam_group.aws_Admins_group.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+resource "aws_iam_user" "MyUser" {
+  name = "MyUser"
+  path = "/"
+}
+
+resource "aws_iam_user_group_membership" "MyUser_to_aws_Admins_group" {
+  user = aws_iam_user.MyUser.name
+
+  groups = [
+    aws_iam_group.aws_Admins_group.name
+  ]
+}
