6e85eae97ae863547ad0a402b06e32f4923d274e
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2022-02-21 15:20:15 +0600

    Create provider.tf for 1-create-administrators-user-group

--- 1-create-administrators-user-group/main.tf
+++ 1-create-administrators-user-group/main.tf
@@ -1,24 +1,9 @@
-provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
-  s3_force_path_style         = false
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    iam            = "http://localhost:4566"
-  }
-}
-
-
 resource "aws_iam_group" "aws_Admins_group" {
   name = "AWSAdminsGroup"
   path = "/"
 }
 
 resource "aws_iam_group_policy_attachment" "aws_group_Admins_attach" {
-  group      = "${aws_iam_group.aws_Admins_group}"
+  group      = aws_iam_group.aws_Admins_group.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
